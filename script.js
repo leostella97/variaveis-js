6b804081daedb0ac418f4cacf182bd1c14063d11
Leonardo Stella de Oliveira <Leonstella97@gmail.com>
2024-09-20 18:34:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+//let cria a variável, pode criar a variável agora e distribuir depois só falando a variável e o que quer armazenar nela
+
+// string
+let texto = "Lorem Ipsum"
+
+//número inteiro
+let numInt = 10
+
+//número com vírgula, chamados de números float
+let numFloat = 5.1
+
+// booleano = valor lógico, true = sim e false = não
+let boolT = true
+let boolF = false
+
+//mostrando as variáveis no console do navegador
+console.log(texto)
+console.log(numInt)
+console.log(numFloat)
+console.log(boolT)
+console.log(boolF)
